34de29d9034e3fa65ef57f86a2e3395bd239a664
Zershyan <1283411677@qq.com>
2026-07-16 02:58:13 +0800

    修复load LocalPlayer导致的服务端报错

--- src/main/java/io/zershyan/manabove/api/ManAboveApi.java
+++ src/main/java/io/zershyan/manabove/api/ManAboveApi.java
@@ -3,15 +3,9 @@ package io.zershyan.manabove.api;
 import io.zershyan.manabove.ManAbove;
 import io.zershyan.manabove.common.registry.MAAttachments;
 import io.zershyan.manabove.common.registry.MASounds;
-import io.zershyan.manabove.config.ClientConfig;
-import io.zershyan.manabove.datagen.init.MATranslatableLang;
 import io.zershyan.manabove.network.data.SoundData;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -89,23 +83,5 @@ public class ManAboveApi {
         if(target instanceof ServerPlayer serverPlayer) {
             PacketDistributor.sendToPlayer(serverPlayer, new SoundData(soundEvent));
         }
-    }
-
-    public void playSound(SoundEvent soundEvent) {
-        if(!entity.level().isClientSide()) return;
-        if(ClientConfig.tipOnFirstTime.getAsBoolean() && entity instanceof Player player) {
-            ClientConfig.tipOnFirstTime.set(false);
-            player.sendSystemMessage(Component.translatable(
-                    MATranslatableLang.SOUND_ENABLED_TIPS.getKey(),
-                    Component.translatable(MATranslatableLang.ENABLE.getKey()).withStyle(style -> style.withClickEvent(
-                            new ClickEvent.RunCommand("/manabove sound enable")
-                    )),
-                    Component.translatable(MATranslatableLang.DISABLE.getKey()).withStyle(style -> style.withClickEvent(
-                            new ClickEvent.RunCommand("/manabove sound disable")
-                    ))
-            ));
-        }
-        if(!ClientConfig.enableSound.getAsBoolean()) soundEvent = SoundEvents.EXPERIENCE_ORB_PICKUP;
-        entity.level().playPlayerSound(soundEvent, SoundSource.PLAYERS, 1.0f, 1.0f);
     }
 }
--- src/main/java/io/zershyan/manabove/network/handler/ClientPayloadHandler.java
+++ src/main/java/io/zershyan/manabove/network/handler/ClientPayloadHandler.java
@@ -1,10 +1,16 @@
 package io.zershyan.manabove.network.handler;
 
-import io.zershyan.manabove.api.ManAboveApi;
+import io.zershyan.manabove.config.ClientConfig;
+import io.zershyan.manabove.datagen.init.MATranslatableLang;
 import io.zershyan.manabove.network.data.SoundData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public class ClientPayloadHandler {
@@ -15,7 +21,21 @@ public class ClientPayloadHandler {
             LocalPlayer player = instance.player;
             if(level == null) return;
             if(player == null) return;
-            ManAboveApi.get(player).playSound(sound.soundEvent());
+            if(ClientConfig.tipOnFirstTime.getAsBoolean()) {
+                ClientConfig.tipOnFirstTime.set(false);
+                player.sendSystemMessage(Component.translatable(
+                        MATranslatableLang.SOUND_ENABLED_TIPS.getKey(),
+                        Component.translatable(MATranslatableLang.ENABLE.getKey()).withStyle(style -> style.withClickEvent(
+                                new ClickEvent.RunCommand("/manabove sound enable")
+                        )),
+                        Component.translatable(MATranslatableLang.DISABLE.getKey()).withStyle(style -> style.withClickEvent(
+                                new ClickEvent.RunCommand("/manabove sound disable")
+                        ))
+                ));
+            }
+            SoundEvent soundEvent = sound.soundEvent();
+            if(!ClientConfig.enableSound.getAsBoolean()) soundEvent = SoundEvents.EXPERIENCE_ORB_PICKUP;
+            player.level().playPlayerSound(soundEvent, SoundSource.PLAYERS, 1.0f, 1.0f);
         });
     }
 }
